43cb678faecef6bd73435beec7d23fd32912c49c
Oliver Bleen <173106526+OliverBleen@users.noreply.github.com>
2025-01-22 17:54:37 +0100

    Make code sidenotes small, finish Bootscreen instructions

--- projects/index.php
+++ projects/index.php
@@ -65,7 +65,7 @@
         <h2>Fursuit Glasses</h2>
         <img src="files/render_glasses.png"></img>
         <p>Because I wanted to have some sort of accessory for my fursuit, I designed and 3D-Printed these fursuit glasses.</p>
-        <p class="link-container">Files:
+        <p class="link-container sidenote">Files:
           <a href="files/fursuit_glasses_frame.stl">STL (Frame)</a>,
           <a href="files/fursuit_glasses_earpiece.stl">STL (Earpiece)</a>,
           <a href="files/fursuit_glasses_frame.stp">STEP (Frame)</a>,
@@ -74,6 +74,9 @@
       <div id="Fursuit_Stand" class="image-height-limited">
         <h2>Fursuit Stand</h2>
         <img src="files/render_stand.png"></img>
+        <p>
+          I designed this fursuit stand so I can easily dry my head after washing it. The left part goes on top of the right part. The base also has a fan builtin that can blow air up into the top part. The fan can be powered using a USB-C cable.
+        </p>
       </div>
       <div id="3D_Printer_Bootscreen" class="image-width-limited">
         <h2>3D Printer Bootscreen</h2>
@@ -82,7 +85,7 @@
         <p class="link-container">For my 3D printer, I compiled and flashed a custom build of <a href="https://marlinfw.org/">Marlin</a>. One of the changes I made was this custom bootscreen that shows up every time the printer starts</p>
         <p class="sidenote">Sidenote: If you add a custom bootscreen to Marlin, normally the Marlin bootscreen shows
           as well. If you want to only show you custom bootscren you need to do the following things (For Marlin v2.1.2.2):</br>
-          Define both <a class="code">SHOW_BOOTSCREEN</a> and <a class="code">SHOW_CUSTOM_BOOTSCREEN</a> (<a class="code">Marlin/Configuration.h</a>, Line 80):
+          Define both <a class="code sidenote">SHOW_BOOTSCREEN</a> and <a class="code sidenote">SHOW_CUSTOM_BOOTSCREEN</a> (<a class="code sidenote">Marlin/Configuration.h</a>, Line 80):
         </p>
 
         <p class="code sidenote">
@@ -93,7 +96,7 @@
           <a class="define">#define SHOW_CUSTOM_BOOTSCREEN</a></br>
         </p>
 
-        <p class="sidenote">Then, in the file <a class="code">Marlin/src/lcd/dogm/marlinui_DOGM.cpp</a>, Line 247, you have to change the function:</p>
+        <p class="sidenote">Then, in the file <a class="code sidenote">Marlin/src/lcd/dogm/marlinui_DOGM.cpp</a>, Line 247, you have to change the function:</p>
         <p class="code sidenote">
           <a class="type">void</a> <a class="class">MarlinUI</a>::<a class="method">show_bootscreen</a><a class="parenthesis">() {</a></br>
           &nbsp&nbsp<a class="method">TERN_</a><a class="parenthesis">(</a><a class="define">SHOW_CUSTOM_BOOTSCREEN</a>, <a class="method">show_custom_bootscreen</a><a class="parenthesis">())</a>;</br>
@@ -110,6 +113,20 @@
           <a class="parenthesis">}</a>
         </p>
 
+        <p class="sidenote">
+          (you can leave out the comments)</br>
+          Now, only the custom bootscreen will show up. But, it seems to display way too long (~5 seconds).</br>
+          To fix this, I changed both the <a class="code sidenote">BOOTSCREEN_TIMEOUT</a> and the <a class="code sidenote">CUSTOM_BOOTSCREEN_TIMEOUT</a>
+        </p>
+        <p class="sidenote">
+          <a class="code sidenote">#define BOOTSCREEN_TIMEOUT 250 // (ms) Total Duration to display the boot screen(s)</a> (File: <a class="code sidenote">Marlin/Configuration_adv.h</a>, Line 1442)
+        </p>
+        <p class="sidenote">
+          <a class="code sidenote">#define CUSTOM_BOOTSCREEN_TIMEOUT 250</a> File: <a class="code sidenote">Marlin/src/lcd/dogm/marlinui_DOGM.cpp</a>, Line 172)<
+        </p>
+        <p class="sidenote">
+          (Despite what the comment in <a class="code sidenote">BOOTSCREEN_TIMEOUT</a> says, from testing it seems that these two timeouts add up. Also, I haven't tried to set one of the Timeouts to 0, as I thought that that might break some stuff.)</br>
+        </p>
       </div>
     </div>
 
